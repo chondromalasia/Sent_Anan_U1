059df62834256df7c90db63fd01db819c934730b
Heath Gordon <angrydogproductions@gmail.com>
2016-03-12 20:30:32 +0100

    Initialize repository

---
+++ uebung_1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#Sentiment Analyse und Media Monitoring
+#### NAME ####
+### DATUM #####
+
+
+
+################
+
+
+
+
+def get_documents():
+	
+	
+
+def get_features(documents):
+		
+	
+	
+def get_more_features(documents):
+	
+
+def featuresets(features, documents):
+
+
+def train_test(features_sets, documents):
+	
+    
+def main():
+	
+	
+	
+if __name__ == '__main__':
+    main()
+
